a66c6e8530ebca8e4c505b4bd1acd58fa476eebe
TFoster16 <northwesterntim@gmail.com>
2025-02-20 17:47:47 -0600

    program_5.py

--- program_5.py
+++ program_5.py
@@ -1,20 +1,38 @@
-# Program #5: Bank Balance
-# Write a program that asks the user to enter the amount that he or she has budgeted for a month.
-# A loop should then prompt the user to enter each of his or her expenses for the 
-# month and keep a running total. (Enter 0 to exit the loop)  
-# When the loop finishes, the program should display the amount that the 
-# user is over or under budget.
+#Timothy Foster, 2/19/25, Bank Balance Program
 
 def main():
+
+    #Define all of the variables that will be used.
     budget = 0.0
     difference = 0.0
     spent = 1.0         #initialize for while loop
     total = 0.0
 
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+    #Ask for budget for the month.
+    budget = int(input ("What is your budget for this month?"))
 
+    #Start while loop.
+    while spent == 1.0:
+
+        #Get the name of an expense.
+        expenseName = input("What is the name of an expense?")
+
+        #Ask for the cost of the expense.
+        total = total + int(input(f"How much does {expenseName} cost you?"))
+
+        #See if user has more expenses to add.
+        spent = int(input("Do you have more expenses for this month? Enter 1 if so and 0 if not."))
+
+    #Calculate if the user if under/over/on budget.
+    difference = int(budget - total)
+
+    #Print the results based on the difference.
+    if difference > 0:
+        print(f"You are under budget by {difference}.")
+    elif difference == 0:
+        print("You are exactly on budget.")
+    else:
+        print(f"You are over budget by {difference//-1}.")
 
 if __name__ == '__main__':
     main()
